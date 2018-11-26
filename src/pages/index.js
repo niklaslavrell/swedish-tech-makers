ace974ac6c2378d03ea8e9afd1b434edffd75b45
Niklas Lavrell <niklas.lavrell@gmail.com>
2018-11-26 13:19:50 +0700

    adding typeform

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,47 @@ import React from 'react'
 const IndexPage = () => (
   <div>
     <h1>Swedish Tech Makers 🚀</h1>
-      <p>We are a community of tech makers who love to create online products. We reach goals and aspirations by holding each other accountable. We inspire and encourage each other to create and innovate. We help finding inspiring jobs and assignments each other. We work from anywhere in the world.</p>
-      <p><span style={{'fontWeight': 'bold'}}>Wanna join and ship some products? 💪</span><br/>Send an email to <a href='mailto:hi@swedishtechmakers.com?subject=I would love to join you 💪' target='_blank'>hi@swedishtechmakers.com</a> and tell us why.</p>
-      <p><span style={{'fontWeight': 'bold'}}>Recruiter looking for freelancers? 👩‍💻</span><br/>We are a selected group of professionals holding competences across development, UX, product, growth, and more - but most importantly - we get stuff shipped. Send an email to <a href='mailto:hi@swedishtechmakers.com?subject=Can we work together? 👩‍💻' target='_blank'>hi@swedishtechmakers.com</a> to find out more.</p>
-      <p>/<a href='https://www.lavrell.se' target='_blank'>Niklas</a> & <a href='https://www.hannasoderquist.se' target='_blank'>Hanna</a></p>
+    <p>
+      We are a community of tech makers who love to create online products. We
+      reach goals and aspirations by holding each other accountable. We inspire
+      and encourage each other to create and innovate. We help finding inspiring
+      jobs and assignments each other. We work from anywhere in the world.
+    </p>
+    <p>
+      <span style={{ fontWeight: 'bold' }}>
+        Wanna join and ship some products? 💪
+      </span>
+      <br />
+      Just{' '}
+      <a href="https://niklaslavrell.typeform.com/to/TNt1WT">
+        fill in this form
+      </a>{' '}
+      and we'll be in touch!
+    </p>
+    <p>
+      <span style={{ fontWeight: 'bold' }}>
+        Recruiter looking for freelancers? 👩‍💻
+      </span>
+      <br />We are a selected group of professionals holding competences across
+      development, UX, product, growth, and more - but most importantly - we get
+      stuff shipped. Send an email to{' '}
+      <a
+        href="mailto:hi@swedishtechmakers.com?subject=Can we work together? 👩‍💻"
+        target="_blank"
+      >
+        hi@swedishtechmakers.com
+      </a>{' '}
+      to find out more.
+    </p>
+    <p>
+      /<a href="https://www.lavrell.se" target="_blank">
+        Niklas
+      </a>{' '}
+      &{' '}
+      <a href="https://www.hannasoderquist.se" target="_blank">
+        Hanna
+      </a>
+    </p>
   </div>
 )
 
