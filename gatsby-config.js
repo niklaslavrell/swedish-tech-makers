f8fa46f1bf965e885aa97d5854e265bf969f8f07
Niklas Lavrell <niklas.lavrell@gmail.com>
2018-12-16 12:28:17 +0800

    adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: 'Swedish Tech Makers'
+    title: 'Swedish Tech Makers',
   },
-  plugins: ['gatsby-plugin-react-helmet',
+  plugins: [
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
-    }
-  ]
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-117499442-4',
+      },
+    },
+  ],
 }
